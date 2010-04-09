5af403f56f503bd717ce67b72067bb8e7dce36dd
Tobias Crawley <tcrawley@gmail.com>
2010-04-09 13:11:48 -0400

    Switched to using ruby's builtin abbrev instead of rolling my own.

--- lib/script_finder/script_finder.rb
+++ lib/script_finder/script_finder.rb
@@ -1,4 +1,5 @@
 require 'ftools'
+require 'abbrev'
 
 module ScriptFinder
   DEFAULT_BIN_DIR = 'script'
@@ -69,31 +70,16 @@ module ScriptFinder
         end
       end
     end
-
-    def unique_prefixes(possibles, prefixes = {})
-      if prefixes.values.uniq.size < possibles.size
-        possibles.each do |cmd|
-          prefixes[cmd] ||= ""
-          if prefix_is_not_unique?(cmd, prefixes[cmd], possibles)
-            prefixes[cmd] += cmd[prefixes[cmd].size,1]
-          end
-        end
-        unique_prefixes(possibles, prefixes)
-      else
+    
+    def unique_prefixes(possibles)
+      all_prefixes = possibles.abbrev
+      all_prefixes.keys.sort.inject({ }) do |prefixes, abbrev|
+        prefixes[all_prefixes[abbrev]] ||= abbrev
         prefixes
       end
     end
-
-    protected
     
-    def prefix_is_not_unique?(current_cmd, current_prefix, possibles)
-      other_possibles = possibles.clone
-      other_possibles.delete(current_cmd)
-
-      (current_prefix.empty? or
-       other_possibles.inject(false) {|accum, c| accum || c[0,current_prefix.length] == current_prefix}) and
-        current_cmd.length > current_prefix.length
-    end
+    protected
     
     def bin_dir_exists_in_pwd?
       File.exists?(bin_dir) and
