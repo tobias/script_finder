901baad23234408450d80ea31dcc4a27c98e8e45
Blue-Dog-Archolite <Blue.Dog.Archolite@gmail.com>
2010-12-23 12:58:27 -0600

    Creation of test class to test functionality moved out of script finder. RRM-BDA

--- test/script_finder_test.rb
+++ test/script_finder_test.rb
@@ -1,21 +1,5 @@
 require 'test_helper'
 
 class ScriptFinderTest < Test::Unit::TestCase
-  
-  def test_unique_prefixes
-    prefixes = ScriptFinder::Finder.new([]).unique_prefixes( %w{db dbconsole destroy desmond})
-    assert_equal prefixes.values.size, prefixes.values.uniq.size
-  end
-  
-  def test_unique_prefixes_gives_shortest_prefix
-    prefixes = ScriptFinder::Finder.new([]).unique_prefixes(%w{db dbconsole destroy desmond funball})
-    assert_equal 'db', prefixes['db']
-    assert_equal 'dbc', prefixes['dbconsole']
-    assert_equal 'dest', prefixes['destroy']
-    assert_equal 'desm', prefixes['desmond']
-    assert_equal 'f', prefixes['funball']
-    
-  end
-  
-  
+
 end
